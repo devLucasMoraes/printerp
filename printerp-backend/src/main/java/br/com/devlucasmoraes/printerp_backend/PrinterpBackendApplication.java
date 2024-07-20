b8ea9908b711cc3cf3803567c35f94c9aaa5ee1b
devLucasMoraes <devlucasmoraes@gmail.com>
2024-07-20 16:29:59 -0300

    Commit inicial

---
+++ printerp-backend/src/main/java/br/com/devlucasmoraes/printerp_backend/PrinterpBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.devlucasmoraes.printerp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinterpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinterpBackendApplication.class, args);
+	}
+
+}
---
+++ printerp-backend/src/test/java/br/com/devlucasmoraes/printerp_backend/PrinterpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devlucasmoraes.printerp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinterpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
